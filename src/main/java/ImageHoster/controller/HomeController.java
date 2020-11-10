69d7e78acf5d8a74c77ec4033f5a63f500c8b115
Prabh <prabhoz@yahoo.com.au>
2020-11-10 12:57:34 +1030

    Stage 2 Display Images

--- src/main/java/ImageHoster/controller/HomeController.java
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -1,14 +1,30 @@
 package ImageHoster.controller;
 
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public  String getAllImages(){
-        return "index";
+    //Write the code to inject the ImageService bean
+    @Autowired
+    private ImageService imageService;
 
+    @RequestMapping("/")
+    public String getAllImages(Model model) {
+        //Call getAllImages() method in ImageService class to get the list of all images
+
+        List<Image> images =imageService.getAllImages();
+        //Add the list of images in the model with the key as "images"
+
+        model.addAttribute("images", images);
+
+        return "index";
     }
 }
